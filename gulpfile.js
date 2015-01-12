158a1ce035a13fbfce91e0380548aeb0d4e04567
Bobby Kolev <bobbykolev@abv.bg>
2015-01-12 18:40:55 +0200

    sass and gulp added, tasks for json minify and sass compile...

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     //gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
-    jsonminify = require('gulp-jsonminify');
+    jsonminify = require('gulp-jsonminify'),
+    sass = require('gulp-sass');
 
 gulp.task('minifyPlaces', function(){
     return gulp.src('places.json')
@@ -10,4 +11,15 @@ gulp.task('minifyPlaces', function(){
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['minifyPlaces'], function(){});
+gulp.task('compileSass', function(){
+    return gulp.src('./sass/style.scss')
+        .pipe(sass())
+        .on('error', function (err) { console.log("[SASS]" + err.message); })
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  	gulp.watch('./sass/**/*.scss', ['compileSass']);
+});
+
+gulp.task('default', ['minifyPlaces', 'compileSass', 'watch'], function(){});
