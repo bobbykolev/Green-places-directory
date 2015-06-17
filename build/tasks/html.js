699d3e3d9d8ff702245d574e649712d4e7a7e6ab
bobbykolev <bobbykolev@abv.bg>
2015-06-17 09:39:14 +0300

    versioning of files added (to disable cached files on client devices)

--- build/tasks/html.js
+++ build/tasks/html.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
+var ver = require('gulp-version-append');
 var paths = require('../paths');
 
 gulp.task('html', function(){
     return gulp.src(paths.html)
+        .pipe(ver(['html', 'js', 'css']))
         .pipe(gulp.dest(paths.output));
 });
