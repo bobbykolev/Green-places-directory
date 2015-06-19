f71d6dcc578742ec112b940e5cf710bd180a20b1
bobbykolev <bobbykolev@abv.bg>
2015-06-19 11:20:43 +0300

    tags end types jsona was added

--- build/tasks/json.js
+++ build/tasks/json.js
@@ -3,7 +3,7 @@ var gp_rename = require('gulp-rename');
 var jsonminify = require('gulp-jsonminify');
 var paths = require('../paths');
 
-gulp.task('json', ['minifyPlacesBg', 'minifyPlacesEn'], function(){
+gulp.task('json', ['minifyPlacesBg', 'minifyPlacesEn', 'minifyTagsEn', 'minifyTagsBg', 'minifyTypesEn', 'minifyTypesBg'], function(){
     return gulp.src(['./data/*.json'])
         .pipe(gulp.dest(paths.output));
 });
@@ -19,5 +19,33 @@ gulp.task('minifyPlacesEn', function(){
     return gulp.src('places_en.json')
         .pipe(jsonminify())
         .pipe(gp_rename('places_min_en.json'))
+        .pipe(gulp.dest('./data'));
+});
+
+gulp.task('minifyTagsEn', function(){
+    return gulp.src('places_tags_en.json')
+        .pipe(jsonminify())
+        .pipe(gp_rename('places_tags_min_bg.json'))
+        .pipe(gulp.dest('./data'));
+});
+
+gulp.task('minifyTagsBg', function(){
+    return gulp.src('places_tags_bg.json')
+        .pipe(jsonminify())
+        .pipe(gp_rename('places_tags_min_bg.json'))
+        .pipe(gulp.dest('./data'));
+});
+
+gulp.task('minifyTypesEn', function(){
+    return gulp.src('places_types_en.json')
+        .pipe(jsonminify())
+        .pipe(gp_rename('places_types_min_bg.json'))
+        .pipe(gulp.dest('./data'));
+});
+
+gulp.task('minifyTypesBg', function(){
+    return gulp.src('places_types_bg.json')
+        .pipe(jsonminify())
+        .pipe(gp_rename('places_types_min_bg.json'))
         .pipe(gulp.dest('./data'));
 });
